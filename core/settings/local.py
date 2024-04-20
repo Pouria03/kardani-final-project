a1bb7f98703fa38ee74783c8bf24df351a40b860
Pouria03 <pouriashaigani@gmail.com>
2024-04-20 19:07:54 +0330

    seperate settings.py file for better organaization

---
+++ core/settings/local.py
@@ -0,0 +1,43 @@
+"""
+    Use for development
+"""
+
+
+from core.settings.base import *
+import os
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+# uploads
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ core/settings/production.py
@@ -0,0 +1,25 @@
+"""
+    Use for proeuction
+"""
+
+
+from core.settings.base import *
+import os
+
+
+DEBUG = False
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# django static root path
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
