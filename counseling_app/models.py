241127704f5e9ac10a40f9e29849311aded5b2bf
Pouria03 <pouriashaigani@gmail.com>
2024-04-22 17:45:50 +0330

    add model for counseling_app

--- counseling_app/models.py
+++ counseling_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Counseling(models.Model):
+    title = models.CharField(max_length=75)
+    # TODO: add ckeditor to field below
+    description = models.TextField(max_length=3000)
+
+    def __str__(self):
+        return self.title
