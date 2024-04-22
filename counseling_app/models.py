0bed86be9e11ca9ea7ec5d412e8f58f263f4e3ce
Pouria03 <pouriashaigani@gmail.com>
2024-04-22 18:01:05 +0330

    some docstrings added to counseling_app model

--- counseling_app/models.py
+++ counseling_app/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+
+
 # Create your models here.
 class Counseling(models.Model):
+    """
+        This method represents all counselings that 
+        people can recieve from thsis website's owner 
+    """
     title = models.CharField(max_length=75)
     # TODO: add ckeditor to field below
     description = models.TextField(max_length=3000)
+
 
     def __str__(self):
         return self.title
