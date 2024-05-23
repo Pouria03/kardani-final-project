e9579e7283038ba4b8e719757ec980dab3aa783f
Pouria03 <pouriashaigani@gmail.com>
2024-05-23 23:30:52 +0330

    modify allowed_hosts and database in settings/production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -8,16 +8,22 @@ import os
 
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*', ]
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'HOST': 'mysql',
+        'USER': 'mysql',
+        'PASSWORD': 'mysql',
     }
 }
+
 
 
 # django static root path
