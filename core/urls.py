18f6d0c558860a32b920fda5fa4f3cc0a7163852
Pouria03 <pouriashaigani@gmail.com>
2024-06-29 14:52:37 +0330

    add robots.txt

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic.base import TemplateView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,7 +12,11 @@ urlpatterns = [
     path('about/', include('about_app.urls')),
     # ckeditor
     path('ckeditor/', include('ckeditor_uploader.urls')),
-
+    # robots.txt
+    path("robots.txt",
+        TemplateView.as_view(template_name="robots.txt",
+                              content_type="text/plain"),
+    ),
 
 ]
 
